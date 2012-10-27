6a9ded382ed95f75689a032f6b68e0735684162d
qc2166 <qc2166@columbia.edu>
2012-10-27 17:34:27 -0400

    find <a
    .

---
+++ src/FindSpecificTags1.java
@@ -0,0 +1,27 @@
+import net.htmlparser.jericho.*;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+public class FindSpecificTags {
+	public static void main(String[] args) throws Exception {
+		String sourceUrlString="http://www.psych.nyu.edu/courses/ugfall12.html";
+		if (sourceUrlString.indexOf(':')==-1) sourceUrlString="file:"+sourceUrlString;
+		MicrosoftConditionalCommentTagTypes.register();
+		MasonTagTypes.register();
+		Source source=new Source(new URL(sourceUrlString));
+		System.out.println("\n*******************************************************************************\n");
+
+		System.out.println("A Elements:");
+		displaySegments(source.getAllElements(HTMLElementName.A));
+  }
+
+	private static void displaySegments(List<? extends Segment> segments) {
+		for (Segment segment : segments) {
+			System.out.println("-------------------------------------------------------------------------------");
+			System.out.println(segment.getDebugInfo());
+			System.out.println(segment);
+		}
+		System.out.println("\n*******************************************************************************\n");
+	}
+}
