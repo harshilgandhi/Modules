443cf640d2d97e0b47c850158f70a56970381208
Harshil Gandhi <harshilgandhi@rocketmail.com>
2012-10-31 21:50:41 +0530

    deliverable for Oct 31
    Nothing more that what Tianchen committed last.

--- src/Main.java
+++ src/Main.java
@@ -33,47 +33,44 @@ public class Main {
                if(validCourse)  //if the we consider this is a course
                { String[] segs=j.split(">");
                         
-               if (segs.length!=1)
-               {
-                    //if title is found, look for course description
-                   if(titleFound){                             
-                       if(segs[1].length()>60)
-                       {
-                           if(descCount<5)
-                           {desc=segs[1];
-                           tempDescs[descCount]=desc; 
-                           descCount++;
-                           }
-                           else  //if the the number of description we got is greater than 5, we consider this not a course
-                           {
-                              validCourse=false;
-                           }
-                       }
-                   }
-                                    
-                   //look for title is its not found yet
-                   if(!titleFound){
-                       if(segs[1].length()>10)
-                       { 
-                           if(!segs[1].contains("\n")) //if it contains a new line character, we consider it not a title
-                           {title=segs[1];                        
-                           titleFound=true;}
-                       }
-                   }
-                   
-               }            
+	               if (segs.length!=1)
+	               {
+	                    //if title is found, look for course description
+	                   if(titleFound){                             
+	                       if(segs[1].length()>60)
+	                       {
+	                           if(descCount<5)
+	                           {desc=segs[1];
+	                           tempDescs[descCount]=desc; 
+	                           descCount++;
+	                           }
+	                           else  //if the the number of description we got is greater than 5, we consider this not a course
+	                           {
+	                              validCourse=false;
+	                           }
+	                       }
+	                   }
+	                                    
+	                   //look for title is its not found yet
+	                   if(!titleFound){
+	                       if(segs[1].length()>10)
+	                       { 
+	                           if(!segs[1].contains("\n")) //if it contains a new line character, we consider it not a title
+	                           {title=segs[1];                        
+	                           titleFound=true;}
+	                       }
+	                   }
+	               }            
                }     
             }
-              if(!desc.equals("")&&!titles.contains(title)&&validCourse)                  
-               {
-                   titles.add(title);      
-                   System.out.println("Course Title: "+title);
-                   for(String k: tempDescs)
-                    if(k!=null)
-                       System.out.println("Course Description: "+k);
-               }
+            if(!desc.equals("")&&!titles.contains(title)&&validCourse)                  
+            {
+            	titles.add(title);
+            	System.out.println("Course Title: "+title);
+            	for(String k: tempDescs)
+            		if(k!=null)
+            			System.out.println("Course Description: "+k);
+            }
         }
-
-        
-     }
+	}
 }
